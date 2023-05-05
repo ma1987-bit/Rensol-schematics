e41d172974655ec6eb22685298e789c79973a00e
mahmoudrensol <mahmoud.ahmed@rensol.be>
2023-05-05 17:31:44 +0200

    table schemtics added

---
+++ projects/super-ui-lib/schematics/table-schematics/schema.ts
@@ -0,0 +1,11 @@
+export interface Schema {
+  name: string;
+  appRoot: string;
+  path: string;
+  sourceDir: string;
+  project: string;
+  module: string;
+  export: boolean;
+  model: string
+  
+  }
