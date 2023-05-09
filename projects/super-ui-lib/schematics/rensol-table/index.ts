620cfea11683f0563c8d42c50c554f8e5b3632fb
mahmoudrensol <mahmoud.ahmed@rensol.be>
2023-05-09 12:15:10 +0200

    table schematics is in orde en import naar app module ook

--- projects/super-ui-lib/schematics/rensol-table/index.ts
+++ projects/super-ui-lib/schematics/rensol-table/index.ts
@@ -6,13 +6,17 @@ import {
 import * as JSON5 from 'json5';
 import { FileModel } from './model';
 import { getWorkspace } from '@schematics/angular/utility/workspace';
-import { capitalize } from '@angular-devkit/core/src/utils/strings';
+// import { capitalize } from '@angular-devkit/core/src/utils/strings';
+import {classify }from '@angular-devkit/core/src/utils/strings';
 
 
 import { strings} from '@angular-devkit/core';
 
 import { Schema as MyServiceSchema } from './schema';
-import { addModuleImportToModule } from '@angular/cdk/schematics';
+// import { addModuleImportToModule } from '@angular/cdk/schematics';
+import { addDeclarationToModule } from '@schematics/angular/utility/ast-utils';
+import { InsertChange } from '@schematics/angular/utility/change';
+import * as ts from 'typescript';
 
 
 
@@ -27,19 +31,48 @@ export function rensol_table(options: MyServiceSchema): Rule {
     const appPath = `${project?.sourceRoot}/app`;
 
     const modelFile = `${appPath}/${options.name}/${options.model}`;
+
     const modelBuffer = host.read(modelFile);
+
 
     if (modelBuffer === null) {
       throw new SchematicsException(`Model file ${options.name} does not exist.`);
     }
+    const modulePath = `${appPath}/app.module.ts`;
+    // const recorder = host.beginUpdate(modulePath);
+    const text:any = host.read(modulePath);
+    // const moduleContent:any = text;
+    // host.create(modulePath, moduleContent);
+
+    const source = ts.createSourceFile(
+      modulePath,
+      text?.toString(),
+      ts.ScriptTarget.Latest,
+      true
+    );
+    const updateRecorder = host.beginUpdate(modulePath);
+    const changes = addDeclarationToModule(
+      source,
+      modulePath,
+      `${classify(options.name.replace(/-/g,""))}`,
+      `./${options.name}/${options.name}.component`
+    ) as InsertChange[];
+
+    for (const change of changes) {
+      if (change instanceof InsertChange) {
+        updateRecorder.insertLeft(change.pos, change.toAdd);
+      }
+    }
+    host.commitUpdate(updateRecorder);
+    console.log(host.get(modulePath)?.content.toString())
 
     const modelJson = modelBuffer.toString('utf-8');
     const model = JSON5.parse(modelJson) as FileModel;
 // instead import to module import to declarations
-    addModuleImportToModule(host,
-      `${appPath}/app.module.ts`,
-      `${capitalize(options.name)}Module`,
-      `./${options.name}/${options.name}.module`);
+    // addModuleImportToModule(host,
+    //   `${appPath}/app.module.ts`,
+    //   `${capitalize(options.name)}Module`,
+    //   `./${options.name}/${options.name}.module`);
 
     const templateSource = apply(url('./files'), [
       applyTemplates({
