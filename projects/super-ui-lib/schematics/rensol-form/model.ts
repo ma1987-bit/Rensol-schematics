7d8babbdf585e2decf0d403f36dfe2b223f326ab
mahmoudrensol <mahmoud.ahmed@rensol.be>
2023-05-10 12:46:46 +0200

    new form schematics added

---
+++ projects/super-ui-lib/schematics/rensol-form/model.ts
@@ -0,0 +1,14 @@
+export interface FileModel {
+  Formname: string;
+  Labels: Labels[];
+}
+export interface Labels {
+  selectlabel: string;
+  email: string;
+  huisnumber: string;
+  busnummer: string;
+  gsmnummer: string;
+  geboortedatum: string;
+  submitbtn: string;
+
+}
